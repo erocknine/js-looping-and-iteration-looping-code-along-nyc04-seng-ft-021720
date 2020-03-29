684c5d1770248ed26e79544e45874c2395c881ef
Sir Eric <echeungsk@gmail.com>
2020-03-29 20:55:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 // Code your solutions in this file
 
 function writeCards(array, occasion){
-  if (occasion === `birthday` || `Birthday`){
-    for (let i = 0; i < array.length; i++){
+  for (let i = 0; i < array.length; i++){
     console.log(`Happy Birthday, ${array[i]}!`)
-  }
-  
   }
 }
